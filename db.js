32c51cde75c5757a1e68497b1c3c1b56f9659525
vimal-eshwaran <118733978+vimal-eshwaran@users.noreply.github.com>
2024-04-12 17:32:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,1 @@
-import mongoose from "mongoose";
 
-
-const databaseconnection = () => {
-    const params = {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }
-    try {
-        mongoose.connect("mongodb+srv://vimalesh114:vimalesh114@cluster0.yxtfffb.mongodb.net/",params)
-        console.log("Mongodb connected")
-    } catch (error) {
-        console.log("Mongodb connection error",error)
-    }
-}
-
-export default databaseconnection;
